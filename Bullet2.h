a3aa8155cf1c77d0356761063a3aa91d9893cbe2
madairnx <hugo2pau@hotmail.fr>
2015-12-06 17:26:23 +0100

    Create Bullet2.h

---
+++ Bullet2.h
@@ -0,0 +1,36 @@
+//
+//  Bullet.h
+//
+
+#ifndef SpriteMove_Bullet_h
+#define SpriteMove_Bullet_h
+
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+class Bullet
+{
+public:
+    //constructor sets ID number, loads sprite
+	Bullet(){};
+	Bullet(const int,float);
+    sf::Sprite & getSprite();
+    void setLocation(float,float);
+    void kill();
+    bool isAlive();
+    void spawn(bool);
+	float getSpeed();
+    //draw the sprite
+    void draw(sf::RenderWindow &win);
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    bool alive;
+	float speed;
+
+};
+
+#endif
